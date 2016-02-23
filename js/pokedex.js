6ff56995c9d18d5dcdc305dbcf0e9d7ea0b4ff34
13010759 <13010759@e007m07.istic.univ-rennes1.fr>
2016-02-23 18:21:07 +0100

    partie 1

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,21 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
+var pokeApp = angular.module('pokedex', ['ngResource'])
+.controller('Ctrl', function($scope) {
+  $scope.test = [
+        {name:'pikachu',id:'1'},
+        {name:'miew',id:'2'},
+        {name:'Kai',id:'3'},
+		{name:'san',id:'4'}
+    ];
+   $scope.id2;
+})
+.controller('Ctrl2', ['$scope', '$log', function($scope, $log) {
+  $scope.$log = $log;
+  $scope.message = 'Hello World!';
+}]);
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
-
+ 
 var pokeApiUrl = "http://pokeapi.co/"
+
