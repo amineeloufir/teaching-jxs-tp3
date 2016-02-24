90613cc3ed5162d44dfea80f87339e5af03e0cf7
13010759 <13010759@e107m04.istic.univ-rennes1.fr>
2016-02-24 18:14:55 +0100

    commit v2

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,18 +1,30 @@
 var pokeApp = angular.module('pokedex', ['ngResource'])
-.controller('Ctrl', function($scope) {
+.controller('Ctrl', function($scope,$log,$http,$resource) {
   $scope.test = [
         {name:'pikachu',id:'1'},
         {name:'miew',id:'2'},
         {name:'Kai',id:'3'},
 		{name:'san',id:'4'}
     ];
-   $scope.id2;
-})
-.controller('Ctrl2', ['$scope', '$log', function($scope, $log) {
-  $scope.$log = $log;
-  $scope.message = 'Hello World!';
-}]);
+	$scope.$log = $log;
+    
+	$resource("http://pokeapi.co/api/v1/type/:id/")
+ 
+	var Pok = $resource('http://pokeapi.co/api/v1/type/:id/');
+	function recupPokemonById(idPokemon, callback) {
+		 var res = Pok.get({id: idPokemon}, function() {
+			callback(res);
+		 });
+	 }
+	 
+	 $scope.goSearch = function() {
+		recupPokemonById($scope.name2, function(res) {
+			$log.info(res);
+		});
+	 };
 
+  
+});
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
